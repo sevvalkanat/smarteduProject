5bdef9ca29c57abecddcc4ff1901e25f9b64d636
sevvalkanat <skanat93@gmail.com>
2024-06-07 00:31:54 +0300

    part2

---
+++ app.js
@@ -0,0 +1,31 @@
+const { setEngine } = require('crypto');
+const express = require('express');
+
+
+const app = express()
+
+//template engine
+app.set("view engine","ejs");
+
+//middlewares
+app.use(express.static("public"))
+
+
+//routes
+ app.get('/',(req,res)=>{
+ res.status(200).render('index',{
+    page_name : "index"
+ })
+ });
+
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{
+        page_name : 'about'
+    });
+});
+
+const post = 3000
+app.list(port,()=>{
+    console.log(`app started on port ${port}`)
+})
+
