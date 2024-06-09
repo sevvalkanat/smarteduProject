d0ddb0cf613bea31ec0c0b8d777a07e90ff1228b
sevvalkanat <skanat93@gmail.com>
2024-06-09 23:52:26 +0300

    part 3

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const { setEngine } = require('crypto');
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute')
 
 const app = express()
+
+//CONNECT DB
+mongoose.connect('mongodb://localhost/smartedu-db'
+).then(()=>{
+    console.log('DB connected succescfuly')
+})
 
 //template engine
 app.set("view engine","ejs");
 
 //middlewares
 app.use(express.static("public"))
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 //routes
- app.get('/',(req,res)=>{
- res.status(200).render('index',{
-    page_name : "index"
- })
- });
+app.use('/',pageRoute);
+app.use('/courses',courseRoute);
 
-app.get('/about',(req,res)=>{
-    res.status(200).render('about',{
-        page_name : 'about'
-    });
-});
-
-const post = 3000
-app.list(port,()=>{
+const port = 3000
+app.listen(port,()=>{
     console.log(`app started on port ${port}`)
 })
 
---
+++ models/Course.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CourseSchema = Schema({
+    name:{
+        type:String,
+        unique: true,
+        required:true
+
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+        
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
+
+
+
+})
+
+const Course = mongoose.model('Course',CourseSchema);
+module.exports = Course;
