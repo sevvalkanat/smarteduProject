d970f8df763c68f8c66cd3ad28cdccbf37963341
sevvalkanat <skanat93@gmail.com>
2024-06-11 13:08:17 +0300

    part 4

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,14 +1,15 @@
 const Course = require('../models/Course')
 
 exports.createCourse = async (req, res) => {
-    const course = await Course.create(req.body);
+    
     try {
+        const course = await Course.create(req.body);
         res.status(201).json({
             status: 'success',
             course
         })
     }
-    catch {
+    catch(error) {
         res.status(400).json({
             status: 'fail',
             error
@@ -16,4 +17,23 @@ exports.createCourse = async (req, res) => {
 
     }
 
-}  
+};
+
+exports.getAllCourses = async (req,res)=>{
+    try{  
+         const courses = await Course.find();
+         res.status(200).render('courses',{
+           courses,
+           page_name: 'courses'
+        });
+
+
+    }
+    catch(error){
+        res.status(400).json({
+            status: 'fail',
+            error
+        })
+
+    }
+ }
