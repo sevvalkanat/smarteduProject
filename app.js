a216a7702ad247dd910a42fc9ce606062f4c83cb
sevvalkanat <skanat93@gmail.com>
2024-06-13 23:47:49 +0300

    part7

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const userRoute = require('./routes/userRoute')
+
 
 const app = express()
 
@@ -17,18 +19,32 @@ mongoose.connect('mongodb://localhost/smartedu-db'
 //template engine
 app.set("view engine","ejs");
 
+//global variable
+global.userIN = null;
+
 //middlewares
 app.use(express.static("public"))
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session({
+    secret: 'my_keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    
+  }))
+ 
 
 
 //routes
+app.use('*',(req,res,next)=>{
+    userIN = req.session.userID;
+    next();
+});
 app.use('/',pageRoute);
 app.use('/courses',courseRoute);
 app.use('/categories',categoryRoute);
 app.use('/users',userRoute);
-//app.use('/login',userRoute); 
+ 
 
 
 const port = 3000
